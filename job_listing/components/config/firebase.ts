c084a5d28f5aa05ab84a971273192da190e27857
KarunaShrestha02 <168328218+KarunaShrestha02@users.noreply.github.com>
2024-12-20 20:03:59 +0545

    Update firebase.ts

--- job_listing/components/config/firebase.ts
+++ job_listing/components/config/firebase.ts
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNjG97t6HhRi_IAXh2k-U3TbBlf3tupWg",
-  authDomain: "job-listing-application-b0aa7.firebaseapp.com",
-  databaseURL: "https://job-listing-application-b0aa7-default-rtdb.firebaseio.com",
-  projectId: "job-listing-application-b0aa7",
-  storageBucket: "job-listing-application-b0aa7.firebasestorage.app",
-  messagingSenderId: "343590740082",
-  appId: "1:343590740082:web:2ec17860a90407fc14a2eb",
-  measurementId: "G-YC1V3M305R"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
